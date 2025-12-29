d2eb86e000b61cf5e80741c3c03f3a2af9f94503
tim stanford <tstanford@gmail.com>
2025-12-29 21:32:50 +0000

    Initial commit

---
+++ agileboard-service/src/main/java/io/stanford/tim/agileboard_service/AgileboardServiceApplication.java
@@ -0,0 +1,13 @@
+package io.stanford.tim.agileboard_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileboardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileboardServiceApplication.class, args);
+	}
+
+}
---
+++ agileboard-service/src/test/java/io/stanford/tim/agileboard_service/AgileboardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.stanford.tim.agileboard_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileboardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
